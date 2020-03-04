aeded4acb690d77be224a663561b407af03a4214
Rob Rastelli <rrastelli@pcgus.com>
2020-03-04 09:38:21 -0500

    Updates for Module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1>{{title}}</h1>
+        <ul>
+            <li v-for="book in books" :key="book.id">
+                {{ book.title }}:{{ book.author }}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -9,14 +14,27 @@
         name: 'BookList',
         data() {
             return {
-                title: 'All Books'
+                title: 'All Books',
+                books: [
+                    { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+                    { title: 'American Gods', author: 'Neil Gaiman' },
+                    { title: 'Amusing Ourselves to Death', author: 'Neil Postman' }
+                ]
             }
         },
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 h1, h2 {
     font-weight: normal;
+}
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    display: block;
+    margin: 0 10px;
 }
 </style>
